6253bcc7615730e2b503b6222b09c68cbff415fb
vinay jadhav <vinayjadhav969@gmail.com>
2026-07-06 16:09:36 +0530

    Added env/dev for compute reosurces

---
+++ Terraform-Infra/Environments/ap-south-1/compute/dev/backend.tf
@@ -0,0 +1,11 @@
+#terraform {
+#backend "s3" {
+#bucket         = "vin-ultimate-bucket" //provide your unique bucket name
+#region         = "ap-south-1"
+#key            = "compute/dev/terraform.tfstate"
+# dynamodb_table = "terraform-locks"
+#  encrypt        = true
+# }
+#}
+
+// create dynamo db table and bucket in s3 before applying it to infra..
